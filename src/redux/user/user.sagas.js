2939c6c3dc871af0eaa2e77f96e9e883a374fd37
Trevor Marlowe <marlowetrevor@hotmail.com>
2019-11-11 15:54:41 -0700

    Converted signOut method into saga

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -2,12 +2,18 @@ import { takeLatest, put, all, call } from 'redux-saga/effects';
 
 import UserActionTypes from './user.types';
 
-import { SignInSuccess, SignInFailure } from './user.actions';
+import {
+  SignInSuccess,
+  SignInFailure,
+  signOutSuccess,
+  signOutFailure
+} from './user.actions';
 
 import {
   auth,
   googleProvider,
-  createUserProfileDocument
+  createUserProfileDocument,
+  getCurrentUser
 } from '../../firebase/firebase.utils';
 
 export function* getSnapshotFromUserAuth(userAuth) {
@@ -37,6 +43,24 @@ export function* signInWithEmail({ payload: { email, password } }) {
     put(SignInFailure(error));
   }
 }
+export function* isUserAuthenticated() {
+  try {
+    const userAuth = yield getCurrentUser();
+    if (!userAuth) return;
+    yield getSnapshotFromUserAuth(userAuth);
+  } catch (error) {
+    yield put(SignInFailure(error));
+  }
+}
+
+export function* signOut() {
+  try {
+    yield auth.signOut();
+    yield put(signOutSuccess());
+  } catch (error) {
+    yield put(signOutFailure(error));
+  }
+}
 
 export function* onGoogleSignInStart() {
   yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
@@ -46,6 +70,19 @@ export function* onEmailSignInStart() {
   yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail);
 }
 
+export function* onCheckUserSession() {
+  yield takeLatest(UserActionTypes.CHECK_USER_SESSION, isUserAuthenticated);
+}
+
+export function* onSignOutStart() {
+  yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut);
+}
+
 export function* userSagas() {
-  yield all([call(onGoogleSignInStart), call(onEmailSignInStart)]);
+  yield all([
+    call(onGoogleSignInStart),
+    call(onEmailSignInStart),
+    call(isUserAuthenticated),
+    call(onSignOutStart)
+  ]);
 }
