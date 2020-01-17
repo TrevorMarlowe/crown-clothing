d7cd2549611a66c59388c2154d3b865082c49e2e
Trevor Marlowe <marlowetrevor@hotmail.com>
2020-01-16 20:34:49 -0700

    adding performance optimizations to codebase with react lazy, suspense and memo

--- client/src/components/cart-item/cart-item.component.jsx
+++ client/src/components/cart-item/cart-item.component.jsx
@@ -4,7 +4,7 @@ import {
   CartItemContainer,
   Image,
   ItemDetailsContainer,
-  Name
+  Name,
 } from './cart-item.styles';
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
@@ -19,4 +19,4 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
   </CartItemContainer>
 );
 
-export default CartItem;
+export default React.memo(CartItem);
